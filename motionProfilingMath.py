4f36ad372e8ad8e0685262d0377dca091ffd9140
dansman805 <dansman805@gmail.com>
2018-03-08 12:51:44 -0500

    Finally functional reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- motionProfilingMath.py
+++ motionProfilingMath.py
@@ -1,59 +1,82 @@
 import matplotlib.pyplot as plt
 from enum import Enum
+import csv
+import numpy as np
 
-class States(Enum):
-    ACCLERATING = 0
-    COASTING = 1
-    DECELERATING = 2
+nameOfFile = input('What do you want to name the output file?\n')
 
-maxSpeed = 152 #In cm/second
-maxAccel = 60 #In cm/second/second 
+outputFile = open(nameOfFile + '.csv', 'w', newline='')
+outputWriter = csv.writer(outputFile)
+
+maxSpeed = 60 #In cm/second
+maxAccel = 30 #In cm/second/second 
 target = 2000 #In cm
 
 
 secondsElapsed = 0
 currentPosition = 0
 currentSpeed = 0
-
-currentState = States.ACCLERATING
+currentAccel = 0
 
 timeList = []
 positionList = []
 speedList = []
+accelList = []
 
-timePerIteration = 0.1 # In seconds
+class States(Enum):
+    ACCLERATING = 0
+    COASTING = 1
+    DECELERATING = 2
+
+currentState = States.ACCLERATING
+
+timePerIteration = 0.0001 # In seconds
 
 
-while currentPosition < target:
-    if currentSpeed >= maxSpeed: 
-        currentState == States.COASTING
+while currentPosition <= target:
+    #Update State
+    distanceToDecelerate = (currentSpeed ** 2) / (2 * maxAccel) 
+    distanceToTarget = target - currentPosition
     
-
-    if currentPosition >= target - (currentSpeed/timePerIteration):
-        currentState == States.DECELERATING
-    
-    if currentState == States.ACCLERATING:
-        currentSpeed += maxAccel * timePerIteration
-
-    if currentState == States.DECELERATING:
-        currentSpeed -= maxAccel * timePerIteration
-
+    #Update values
     secondsElapsed += timePerIteration
     currentPosition += currentSpeed * timePerIteration
+    
+    if distanceToTarget <= distanceToDecelerate:
+        currentState = States.DECELERATING
+    elif currentSpeed < maxSpeed:
+        currentState = States.ACCLERATING
+    else:
+        currentState = States.COASTING
 
-    print('Seconds Elapsed: ' + str(secondsElapsed))
-    print('Current Pos: ' + str(currentPosition))
-    print('Current Speed: ' + str(currentSpeed))
+    #Update accleration based on state
+    if currentState == States.ACCLERATING: currentAccel = maxAccel    
+    if currentState == States.COASTING: currentAccel = 0
+    if currentState == States.DECELERATING: currentAccel = -maxAccel
 
+    currentSpeed += currentAccel * timePerIteration
+
+    #Add values to array
     timeList += [secondsElapsed]
     positionList += [currentPosition]
     speedList += [currentSpeed]
+    accelList += [currentAccel]
 
+    #Write time, speed, and position to file
+    outputWriter.writerow([secondsElapsed, currentSpeed, currentPosition])
 
+outputFile.close()
+
+#Plot everything
+ax = plt.subplot(111)
+
+ax.set_prop_cycle('color',plt.cm.spectral(np.linspace(0,10,40)))
+
+plt.plot(timeList, positionList)
 plt.plot(timeList, speedList)
+plt.plot(timeList, accelList)
 
 plt.xlabel('Time (seconds)')
-plt.ylabel('Speed List')
-
+plt.legend(['Position (cm)', 'Speed (cm/s)', 'Acceleration (cm/s/s)'], loc='upper left')
 
 plt.show()
